162afe806eeff213798440d2353c9d801216975f
Sergey Miroshnichenko <serg.zorg@gmail.com>
2015-03-31 20:49:50 +0300

    delete mod_query

--- modulelist.mk
+++ modulelist.mk
@@ -2,6 +2,6 @@
 ## List of modules to build
 ##
 
-MODULE_LIST = mod_tiling mod_query \
+MODULE_LIST = mod_tiling \
 	      mod_dock mod_sp mod_sm \
 	      de mod_xrandr mod_xkbevents mod_notionflux
