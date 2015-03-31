0b1aa47d0745b41b638f9792d212095afa504d6d
Sergey Miroshnichenko <serg.zorg@gmail.com>
2015-03-31 20:15:10 +0300

    delete mod_statusbar

--- modulelist.mk
+++ modulelist.mk
@@ -3,5 +3,5 @@
 ##
 
 MODULE_LIST = mod_tiling mod_query mod_menu \
-	      mod_dock mod_sp mod_sm mod_statusbar \
+	      mod_dock mod_sp mod_sm \
 	      de mod_xrandr mod_xkbevents mod_notionflux
