a0d5ccb8a2073c3a8ff7cd2e30ce6a15a3638314
Sergey Miroshnichenko <serg.zorg@gmail.com>
2015-04-13 22:19:28 +0300

    force lua51 for now

--- build/lua-detect.mk
+++ build/lua-detect.mk
@@ -1,4 +1,4 @@
-LUA_VERSION=
+LUA_VERSION=5.1
 
 LUA_LIBS     := $(or $(shell pkg-config --libs lua$(LUA_VERSION)),   $(error "pkg-config couldn't find linker flags for lua$(LUA_VERSION)!"))
 LUA_INCLUDES := $(shell pkg-config --cflags lua$(LUA_VERSION))
