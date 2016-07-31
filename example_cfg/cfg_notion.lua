b3a040da831067c23121bf23cdd8dad6f953e875
Neg <serg.zorg@gmail.com>
2016-07-31 10:35:02 +0300

    systemwide cfg_notion

--- example_cfg/cfg_notion.lua
+++ example_cfg/cfg_notion.lua
@@ -1,42 +1,25 @@
-home_        = os.getenv("HOME")
-notion_path_ = home_ .. "/.notion/"
+dopath("mod_notionflux") -- socket-based notion ipc
+dopath("mod_tiling")     -- static tiling notion module
+dopath("mod_xrandr")     -- xrandr support for notion
+dopath("mod_dock")       -- dock support for notion
+dopath("mod_xkbevents")  -- xkbevents support
 
-dofile(notion_path_.."load_file"..".lua")
-
-local mod_list = {
-    "mod_notionflux",    -- socket-based notion ipc
-    "mod_tiling",        -- static tiling notion module
-    "mod_xrandr",        -- xrandr support for notion
-    "mod_dock",          -- dock support for notion
-    "mod_xkbevents",     -- xkbevents support
-}
-
-local cfg_list = { 
-    "dbg",               -- functions for stderr debbuging
-    "cfg_settings",      -- notioncore.set
-    "cfg_rules",         -- window placement rules
-    "cfg_layouts",       -- a bunch of default layouts
-    "app",               -- module to jump by class, tag, instance etc
-    "named_scratchpad",  -- named scratchpad module
-    "hide_tabs",         -- hide tabs
-    "net_client_list",   -- net client list to provide `wmctrl -l`
-    "directions",        -- 2bwm-like directions
-    "screenshot",        -- screenshot script
-    "cfg_dzen",          -- dzen config
-    "rofi",              -- rofi module for menu
-    "cfg_autostart",     -- apps autostart
-    "functions",         -- helpers. Primarily for cfg_kb.lua
-    "cfg_kb",            -- keybindings
-    "cfg_xrandr",        -- xrandr config
-    "transparency",      -- autoset transparency
-    "dzen_bg",           -- background reader for dzen
-    "lua_repl"           -- experimental script standalone lua repl
-}
-
-for _,mod in ipairs(mod_list) do
-    dopath(mod)
-end
-
-for _,mod in ipairs(cfg_list) do
-    load_file(mod)
-end
+dopath("dbg")               -- functions for stderr debbuging
+dopath("cfg_settings")      -- notioncore.set
+dopath("cfg_rules")         -- window placement rules
+dopath("cfg_layouts")       -- a bunch of default layouts
+dopath("app")               -- module to jump by class tag instance etc
+dopath("named_scratchpad")  -- named scratchpad module
+dopath("hide_tabs")         -- hide tabs
+dopath("net_client_list")   -- net client list to provide `wmctrl -l`
+dopath("directions")        -- 2bwm-like directions
+dopath("screenshot")        -- screenshot script
+dopath("cfg_dzen")          -- dzen config
+dopath("rofi")              -- rofi module for menu
+dopath("cfg_autostart")     -- apps autostart
+dopath("functions")         -- helpers. Primarily for cfg_kb.lua
+dopath("cfg_kb")            -- keybindings
+dopath("cfg_xrandr")        -- xrandr config
+dopath("transparency")      -- autoset transparency
+dopath("dzen_bg")           -- background reader for dzen
+dopath("lua_repl")          -- experimental script standalone lua repl
